f4ca2976aaffffe6d4e6ad604a2b0765bd77e742
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-20 12:06:04 -0700

    removed date stamp

--- server.js
+++ server.js
@@ -7,16 +7,10 @@ const path = require("path");
 
 // app.use(express.static("build"));
 
-const today = Date.now();
-
-// app.use("/", (req, res) => {
-//   return res.sendFile(path.join(__dirname + "/build/index.html"));
-// });
-
 app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
-      `https://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
+      `https://newsapi.org/v2/everything?q=crypto&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
     );
 
     res.send({ articles: articles.data.articles });
