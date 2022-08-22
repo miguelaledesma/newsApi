57b7bf389e00100798d26c1755d8782a34dcba39
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-22 13:07:59 -0700

    one of the params in the last commit was wrong, fixing it now

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
-      `https://newsapi.org/v2/everything?q=tesla&language=en&sortBy=publishedAt&apiKey=644b946eec0b49f4ba12589738e0877f`
+      `https://newsapi.org/v2/everything?q=crypto&language=en&sortBy=publishedAt&apiKey=644b946eec0b49f4ba12589738e0877f`
     );
 
     res.send({ articles: articles.data.articles });
