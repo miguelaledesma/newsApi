4be8a30b37bff90306a70473276667d2dd93f801
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-09-11 19:50:58 -0700

    working on env

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
-      `https://newsapi.org/v2/everything?q=crypto&language=en&sortBy=publishedAt&apiKey=644b946eec0b49f4ba12589738e0877f`
+      `https://newsapi.org/v2/everything?q=crypto&language=en&sortBy=publishedAt&apiKey=${process.env.API_KEY_VALUE}`
     );
 
     res.send({ articles: articles.data.articles });
