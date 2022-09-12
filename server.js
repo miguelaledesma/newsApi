f3516723ca06286eb8671526881361bfb687a7c7
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-09-11 19:33:05 -0700

    added api key to env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
@@ -7,7 +8,7 @@ const app = express();
 app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
-      `https://newsapi.org/v2/everything?q=crypto&language=en&sortBy=publishedAt&apiKey=644b946eec0b49f4ba12589738e0877f`
+      `https://newsapi.org/v2/everything?q=crypto&language=en&sortBy=publishedAt&apiKey=${process.env.API_KEY_VALUE}`
     );
 
     res.send({ articles: articles.data.articles });
